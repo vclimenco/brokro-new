da6a057e63f43d6e0254e203621d4bcc86165b52
vclimenco <climenco@gmail.com>
2021-06-23 10:50:49 +0300

    Added queryable worker example

--- public/StockWorker.js
+++ public/StockWorker.js
@@ -1,35 +1,20 @@
-// self.importScripts('http://127.0.0.1:5500/public/StockRepository.js');
-// self.importScripts('http://127.0.0.1:5500/public/StockService.js');
 
 // TODO: add setInterval
-// TODO: add notoification + vibration => that should be done in a wrapper
+// TODO: add notification + vibration => that should be done in a wrapper
 // TODO: add OHLC
-console.log(self);
-//let stockService = new StockService('kava');
 
-class Stock {
-  name = '';
-  buyPrice = 0.0;
-  sellPrice = 0.0;
-}
+const getTicker = function (ticker) {
+  const url = `https://api.kraken.com/0/public/Ticker?pair=${ticker}usd`;
+  // fetch(this.url)
+  //     .then(response => response.json())
+  //     .then(data => console.log(data));
+  let workerResult = 'hi from worker';
+  self.postMessage(workerResult);
+};
 
-class StockService {
-  #ticker = '';
-  url = '';
-  constructor(ticker) {
-      this.#ticker = ticker;
-      this.url = `https://api.kraken.com/0/public/Ticker?pair=${this.#ticker}usd`;
-  }
-
-  getTicker() {
-    fetch(this.url)
-        .then(response => response.json())
-        .then(data => console.log(data));
-  }
-}
-
-//setInterval(stockService.getTicker, 1000);
-setTimeout(() => (new StockService('kava')).getTicker(), 10);
+setTimeout(() => getTicker('kava'), 1000);
+//setTimeout(() => getTicker('kava'), 60 * 1000);
+//setInterval(() => getTicker('kava'), 60 * 1000);
 
 self.onmessage = function(e) {
     console.log(e.data);
