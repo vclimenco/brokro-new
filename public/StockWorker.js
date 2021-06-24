e9db9324b801b8ced104783fdcca5e04ed9953cf
vclimenco <climenco@gmail.com>
2021-06-24 11:10:24 +0300

    Added manifest

--- public/StockWorker.js
+++ public/StockWorker.js
@@ -4,13 +4,32 @@
 // TODO: add active time interval - like 6:00 AM / 23:00 PM
 // TODO: add the other currencies + DB
 // TODO: AI to ddtect the spikes in exc
+// TODO: add the check for the latency on the site
 
-const getTicker = function (ticker) {
-  fetch(`https://api.kraken.com/0/public/Ticker?pair=${ticker}usd`)
-      .then(response => response.json())
-      .then(data => self.postMessage(data));
+const kavaBuyPrice = 3.0;
+const kavaSellPrice = 4.4;
+
+const getTicker = function (crypto) {
+  fetch('/public/sample.json')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .then(() => clearInterval(interval));
+  // let ticker = `${crypto}USD`;
+  // fetch(`https://api.kraken.com/0/public/Ticker?pair=${ticker.toLowerCase()}`)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       var cryptoPrice = parseFloat(data.result[ticker].c[0]);
+  //       if (cryptoPrice <= kavaBuyPrice) {
+  //         self.postMessage(`Buy ${crypto}: ${cryptoPrice}`);
+  //       } else if (kavaSellPrice <= cryptoPrice) {
+  //         self.postMessage(`Sell ${crypto}: ${cryptoPrice}`);
+  //       } else {
+  //         self.postMessage('Stay on alert!');
+  //       }
+ 
+  //     });
 };
 
-setTimeout(() => getTicker('kava'), 1000);
-//setTimeout(() => getTicker('kava'), 60 * 1000);
-//setInterval(() => getTicker('kava'), 60 * 1000);
+let interval = setInterval(() => getTicker('KAVA'), 1000);
+//setTimeout(() => getTicker('KAVA'), 60 * 1000);
+//setInterval(() => getTicker('KAVA'), 60 * 1000);
